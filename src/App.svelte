157cfda24db5966c6ad469680e523a5d995cf9aa
Abhijeet WebDev <abhijeetwebdev@gmail.com>
2024-10-24 12:25:03 +0530

    add: routing and basic styling

--- src/App.svelte
+++ src/App.svelte
@@ -1,1 +1,20 @@
-Hello!
+<script lang="ts">
+  import { Router, Route, Link } from 'svelte-routing'
+  import Home from './routes/Home.svelte'
+  import About from './routes/About.svelte'
+  import News from './routes/News.svelte'
+</script>
+
+<Router>
+  <nav>
+    <Link to="/">Home</Link>
+    <Link to="/about">About</Link>
+    <Link to="/news/1">News Item 1</Link>
+    <Link to="/news/2">News Item 2</Link>
+  </nav>
+  <Route path="/" component={Home} />
+  <Route path="/about" component={About} />
+  <Route path="/news/:id" let:params>
+    <News params={{ id: params.id }} />
+  </Route>
+</Router>
---
+++ src/routes/News.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+  interface NewsParams {
+    id: string
+  }
+
+  export let params: NewsParams
+</script>
+
+<h1>News Item</h1>
+<p>News ID: {params.id}</p>
