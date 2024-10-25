b3d7b5d805dabfdd5f52851b683dca43f1bbda5a
Abhijeet WebDev <abhijeetwebdev@gmail.com>
2024-10-25 08:53:49 +0530

    update: UI improvements

--- src/components/NewsCard.svelte
+++ src/components/NewsCard.svelte
@@ -1,25 +1,45 @@
 <script lang="ts">
+  import { onMount } from 'svelte'
   import type { NewsArticle } from '../interfaces/appInterfaces'
   export let news: NewsArticle
+
+  onMount(() => {})
 </script>
 
-<div class="card mt-4 pointer">
-  <!-- <div
-    class="image-container h-40 bg-cover bg-center rounded-t-md"
+<div class="card mt-4">
+  <div
+    class="image-wrapper h-40 bg-cover bg-center rounded-t-md"
     style="background-image: url('{news.image_url}');"
     aria-label={news.title}
-  ></div> -->
-  <img src={news.image_url} alt="" />
-  <h3 class="pt-2"><strong>{news.title}</strong></h3>
-  <p class="pt-2">{news.content}</p>
+  ></div>
+  <div class="content-wrapper">
+    <!-- <img src={news.image_url} alt="Image: {news.title}" /> -->
+    <h3 class="pt-2"><strong>{news.title}</strong></h3>
+    <p class="pt-2">{news.content}</p>
+  </div>
 </div>
 
 <style>
   .card {
     border: 1px solid #ccc;
-    border-radius: 8px;
-    padding: 16px;
-    box-shadow: 0 2px 10px rgba(0, 0, 0, 0.1);
+    border-radius: 0.5rem;
+    padding: 1rem;
     background-color: white;
+    box-shadow: 0 2px 10px rgba(0, 0, 0, 0.1);
+    width: auto;
+    transition: transform 0.2s;
+    display: flex;
+  }
+
+  .image-wrapper {
+    height: 300px;
+    width: 40%;
+    border-radius: 0.5rem;
+  }
+
+  .content-wrapper {
+    height: 300px;
+    width: 60%;
+    padding: 0 0 0 1rem;
   }
 </style>
--- src/routes/Home.svelte
+++ src/routes/Home.svelte
@@ -1,14 +1,11 @@
 <script lang="ts">
   import { onMount } from 'svelte'
   import appStore, { fetchNews } from '../store/appStore'
-  import type { AppState, NewsArticle } from '../interfaces/appInterfaces'
+  import type { AppState } from '../interfaces/appInterfaces'
   import PreloaderCard from '../components/PreloaderCard.svelte'
   import NewsCard from '../components/NewsCard.svelte'
-  import { chunkArray } from '../utils/helper'
-  import { Link } from 'svelte-routing'
 
   let appState: AppState
-  let groupedNews: NewsArticle[][] = []
 
   onMount(() => {
     if (!appState.loading && appState.news.length == 0) {
@@ -22,49 +19,18 @@
     // reading the app state updates
     appStore.subscribe((value) => {
       appState = value
-      if (appState.news.length > 0) {
-        // grouping the news articles for the masonry tiles UI
-        groupedNews = chunkArray(appState.news, 3)
-      }
     })
   }
 </script>
 
 <div class="container mx-md m-auto p-5">
-  <div class="grid grid-cols-2 md:grid-cols-4 gap-4">
-    {#if appState.loading}
-      {#each Array(20) as _}
-        <div class="grid gap-4">
-          <PreloaderCard></PreloaderCard>
-        </div>
-      {/each}
-    {:else}
-      <!-- <div class="flex flex-wrap gap-4">
-      {#each news as item, index}
-        <NewsCard bind:news={item}></NewsCard>
-      {/each}
-    </div> -->
-      {#each groupedNews as group}
-        <div class="grid gap-4">
-          {#each group as item}
-            <div class="card-wrapper">
-              <Link to="/news/{item.hash_id}">
-                <NewsCard bind:news={item}></NewsCard>
-              </Link>
-            </div>
-          {/each}
-        </div>
-      {/each}
-    {/if}
-  </div>
+  {#if appState.loading}
+    {#each Array(20) as _}
+      <PreloaderCard></PreloaderCard>
+    {/each}
+  {:else}
+    {#each appState.news as item}
+      <NewsCard bind:news={item}></NewsCard>
+    {/each}
+  {/if}
 </div>
-
-<style>
-  .card-wrapper {
-    transition: transform 0.2s;
-  }
-
-  .card-wrapper:hover {
-    transform: scale(1.02);
-  }
-</style>
