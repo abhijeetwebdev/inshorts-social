c960fa480de9aa0916c0e28a0ed1f876744f05d0
Abhijeet WebDev <abhijeetwebdev@gmail.com>
2025-02-02 13:49:25 +0530

    cleanup

--- src/App.svelte
+++ src/App.svelte
@@ -2,7 +2,6 @@
   import { Router, Route } from 'svelte-routing'
   import Home from './routes/Home.svelte'
   import About from './routes/About.svelte'
-  import News from './routes/News.svelte'
   import Header from './components/Header.svelte'
   import Footer from './components/Footer.svelte'
 </script>
@@ -12,9 +11,6 @@
 <Router>
   <Route path="/" component={Home} />
   <Route path="/about" component={About} />
-  <Route path="/news/:id" let:params>
-    <News params={{ id: params.id }} />
-  </Route>
 </Router>
 
 <Footer></Footer>
